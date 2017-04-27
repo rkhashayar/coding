260c07c3c4ee69bb6df86b93c84912e5c3f4ffd7
kxr0918 <kxr0918@CN-DEVELOPER11>
2017-04-27 13:21:27 -0500

    add euler

---
+++ Coding/Euler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your number would be?");
+            long number = long.Parse(Console.ReadLine());
+            Console.WriteLine(CalculateEvenFibo(number).ToString());
+            Console.ReadLine();
+        }
+
+        public static long CalculateEvenFibo(long n)
+        {
+            long fib1 = 1;
+            long fib2 = 1;
+            long result = 0;
+            long summed = 0;
+
+            while (result < n)
+            {
+                if (result % 2 == 0)
+                {
+                    summed += result;
+                }
+                result = fib1 + fib2;
+                fib1 = fib2;
+                fib2 = result;
+            }
+            return summed;
+        }
+    }
+}
