f175e991cc705891af0aa5e993b551d1c79f1a35
rkhashayar <rkhashayar@live.com>
2017-05-20 15:24:14 -0500

    add youtube learnings

--- Coding/ManageMyNotes/Scripts/euler_project.js
+++ Coding/ManageMyNotes/Scripts/euler_project.js
@@ -1,5 +1,5 @@
-﻿var EulerProject = new function() {
-    this.Problem1 = function(x) {
+﻿var EulerProject = new function () {
+    this.Problem1 = function (x) {
         var i = 1;
         var result = 0;
         while (i < x) {
@@ -10,7 +10,7 @@
         }
         return result;
     };
-    this.Problem2 = function(x) {
+    this.Problem2 = function (x) {
         var fib1 = 1;
         var fib2 = 1;
         var temp = 0;
@@ -19,10 +19,28 @@
             temp = fib1 + fib2;
             fib1 = fib2;
             fib2 = temp;
-            if (temp%2 ===0) {
+            if (temp % 2 === 0) {
                 result += temp;
             }
         }
-        return result;
-    }
+        return result + EulerProject.NewEntity;
+    };
+    this.Problem3 = function (x) {
+        var prime = 0;
+        for (var i = 2; i < x; i++) {
+            if (x % i === 0)
+                var isPrime = true;
+            for (var j = 2; j < i; j++) {
+                if (i % j === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                prime = i;
+            }
+        }
+        return prime;
+    };
 }
+EulerProject.NewEntity = "I am new content";
---
+++ Coding/YouTubeLearning/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeLearning
+{
+    class Program
+    {
+        static IList<int> listOfInt = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+        static void Main(string[] args)
+        {
+            foreach (var item in CustomIteration())
+            {
+                Console.WriteLine(item.ToString());
+            }
+            foreach (var item in StatefulIteration())
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.Read();
+        }
+
+        #region Yield Key Word
+        /*
+         * used for 1. custom interation
+         *          2. stateful iteration
+         */
+
+        //  1.
+        static IEnumerable<int> CustomIteration()
+        {
+            foreach (var item in listOfInt)
+            {
+                if (item > 3)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        // 2. stateful
+        static IEnumerable<int> StatefulIteration()
+        {
+            var total = 0;
+            foreach (var item in listOfInt)
+            {
+                total += item;
+                yield return total;
+            }
+        }
+        #endregion
+
+
+
+    }
+}
