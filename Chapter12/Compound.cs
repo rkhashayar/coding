da6f1d1e51cd23b4ddad190dc2d9e69fdf8497b0
khashayar <rkhashayar@live.com>
2019-06-17 06:31:58 -0500

    home

---
+++ Chapter12/Compound.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Chapter12
+{
+    // compound patterns
+    public interface IQuackable
+    {
+        void Quack();
+    }
+    public class RedHeadDuck : IQuackable
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Red Head Quacked");
+        }
+    }
+    public class MallardDuck : IQuackable
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Mallard Duck Quacked");
+        }
+    }
+    public class CallDuck : IQuackable
+    {
+        public void Quack()
+        {
+            Console.WriteLine("this is not a real duck.");
+        }
+    }
+    public class RubberDuck : IQuackable
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Squeak");
+        }
+    }
+}
