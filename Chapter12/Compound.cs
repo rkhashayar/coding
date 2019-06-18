d51fef938e692b903c0e5b63c435a78f29f7f00c
khashayar <rkhashayar@live.com>
2019-06-18 18:13:08 -0500

    work

--- Chapter12/Compound.cs
+++ Chapter12/Compound.cs
@@ -49,14 +49,42 @@ namespace Chapter12
             IQuackable redHeadDuck = new RedHeadDuck();
             IQuackable rubberDuck = new RubberDuck();
             IQuackable duckCall = new CallDuck();
+            IQuackable goose = new DuckAdaptor(new Goose());
             Simulate(mallardDuck);
             Simulate(redHeadDuck);
             Simulate(rubberDuck);
             Simulate(duckCall);
+            Simulate(goose);
         }
         public void Simulate(IQuackable quackable)
         {
             quackable.Quack();
+        }
+    }
+
+    public interface IHonker
+    {
+        void Honk();
+    }
+
+    public class Goose : IHonker
+    {
+        public void Honk()
+        {
+            Console.WriteLine("this is goose Honking");
+        }
+    }
+    // adaptor pattern to be able to make use of quackable for goose
+    public class DuckAdaptor : IQuackable
+    {
+        IHonker _honker;
+        public DuckAdaptor(IHonker honker)
+        {
+            _honker = honker;
+        }
+        public void Quack()
+        {
+            _honker.Honk();
         }
     }
 }
